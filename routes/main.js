9d605221c41ee26a2f5d94d2a764618633ec2545
juneyoung <juneyoung85@gmail.com>
2016-03-04 01:07:37 +0900

    add products(mongoose populate, url path parameter)

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 var router = require('express').Router();
+var Product = require('../models/product');
 
 router.get('/', function(req, res){
 	//res.json('Hello, node!');
@@ -9,5 +10,19 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
 	res.render('main/about');
 });
+
+
+//: means path parameter
+// populate is only abled when id is Schema.Types.ObjectId(mongoose), it access data with given ID
+router.get('/products/:id', function(req, res, next){
+	Product.find({category : req.params.id})
+	.populate('category')
+	.exec(function(err, products){
+		if(err) return next(err);
+		res.render('/main/category', {
+			products : products
+		});
+	})
+})
 
 module.exports = router;
