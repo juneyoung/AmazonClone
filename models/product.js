e571275b73921babeaf77cc8170c2d8e8a13bf61
juneyoung <juneyoung85@gmail.com>
2016-03-06 21:02:16 +0900

    add mongoosastic and elastic search

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
 var mongoose = require('mongoose');
+//ElaticSearch 를 사용하여 기본 모델에 Product.search/craeteMapping 와 같은 방식으로 검색을 할 수 있게 함.
+var mongoosastic = require('mongoosastic');
 var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
@@ -6,6 +8,13 @@ var ProductSchema = new Schema({
 	, name : String
 	, price : Number
 	, image : String
+});
+
+//Set Elastic Search
+ProductSchema.plugin(mongoosastic, {
+	hosts : [
+		'localhost:9200'
+	]
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,38 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+Product.createMapping(function(err, mapping){
+	if(err){
+		console.log('Error create Mapping');
+		console.error(err);	
+	} else {
+		console.log('Mapping created');
+		console.log(mapping);
+	}
+
+});
+
+//Bridge between eleastic replica set and Product database
+//엘라스틱 서치가 모든 데이타 셋을 레플리케이트(복제)함.
+
+var stream = Product.synchronize();
+var count = 0;
+
+//총 데이타 카운팅
+stream.on('data', function(){
+	count++;
+});
+
+//동기화가 종료되었을 때 총합을 출력
+stream.on('close', function(){
+	console.log('Indexed ' + count + ' documents.');
+});
+
+//에러 발생시 에러 출력 
+stream.on('error', function(err){
+	console.error(err);
+});
+
 router.get('/', function(req, res){
 	//res.json('Hello, node!');
 	res.render('main/home');
@@ -10,6 +42,29 @@ router.get('/', function(req, res){
 router.get('/about', function(req, res){
 	res.render('main/about');
 });
+
+
+router.post('/search', function(req, res, next){
+	res.redirect('/search?q=' + req.body.q);
+});
+
+router.get('/search', function(req, res, next){
+	if(req.query.q){
+		Product.search({
+			query_string : {query :  req.query.q}
+		}, function(err, results){
+			if (err) return next(err);
+			var data = results.hits.hits.map(function(hit){
+				return hit;
+			});
+
+			res.render('main/search-result', {
+				query : req.query.q
+				, data : data
+			});
+		});
+	}
+})
 
 
 //: means path parameter
@@ -23,6 +78,16 @@ router.get('/products/:id', function(req, res, next){
 			products : products
 		});
 	})
-})
+});
+
+
+router.get('/product/:id', function(req, res, next){
+	Product.findById({_id : req.params.id }, function(err, product){
+		if(err) return next(err);
+		res.render('main/product', {
+			product : product
+		});
+	});
+});
 
 module.exports = router;
